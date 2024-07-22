beb772325c096a26f1b33e7acff2553d59ac7e4a
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-07-22 23:01:48 +1000

    added eslint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './styles/main.scss';
 
 function component() {
     const element = document.createElement('h1');
-    const btn = document.createElement('button');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
